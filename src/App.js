093fa907b4a300467a3d29bc2b876b57dfeb3645
ZhuQingZhou <3226958502@qq.com>
2019-12-29 11:36:58 +0800

    实现导航功能

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 // 1.引入依赖
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
 // 2.引入css
 import "./App.css"
+import constants from 'jest-haste-map/build/constants'
 
 export default class App extends Component {
-  render(){
-    let title = ['Pull requests','lssues','Marketplace','Explore']
-    let isBoy = true
+
+  state = {
+    titles: ['Pull requests', 'lssues', 'Marketplace', 'Explore'],
+    desc:''
+  }
+  handleClick = (v)=>{
+    this.setState({
+      desc:v
+    })
+  }
+  render() {
+    // let title = ['Pull requests','lssues','Marketplace','Explore']
+    // let isBoy = true
     return <div>
       <ul>
         {
-          title.map((v,i) => <li key={i}>{v}</li>)
+          this.state.titles.map((v, i) => <li onClick={this.handleClick.bind(this,v)} key={i}>{v}</li>)
         }
       </ul>
-      {
-        isBoy?'男':'女'
-      }
+      <div>{this.state.desc}</div>
     </div>
+    // let props = {
+    //   className:'colRed'
+    // }
+    // return React.createElement('div',props,[React.createElement('span',{},'今天'),React.createElement('div',{},this.state.desc)])
   }
 }
